34bd87203accdcce1a1f76b8f9059a3896ebea00
Saikat123Dev <berasaikat729@gmail.com>
2025-07-22 10:00:16 +0530

    basic setup complete

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100 text-red-500">
+      Hello World!
+      <Button>Click Me</Button>
+  </div>
+  );
+}
